16406e3a6568cd04b6151fa75a9f9f95d456480c
idearendil <idltm0208@gmail.com>
2023-01-20 00:24:56 +0900

    basic member variables of state

--- othello.py
+++ othello.py
@@ -1,7 +1,7 @@
 """
 Othello(Reversi) Environment
 
-Coordinates are specified in the form of '(r, c)', where '(0, 0)' is the top left corner.
+Coordinates are specified in the form of ''(r, c)'', where ''(0, 0)'' is the top left corner.
 All coordinates and directions are absolute and does not change between agents.
 
 Directions
@@ -13,6 +13,41 @@ Directions
 
 import sys
 import numpy as np
+
 from numpy.typing import ArrayLike, NDArray
 
+if sys.version_info < (3, 10):
+    from typing_extensions import TypeAlias
+else:
+    from typing import TypeAlias
+
 from fights.base import BaseEnv, BaseState
+
+OthelloAction: TypeAlias = ArrayLike
+"""
+Alias of :obj:'ArrayLike' to describe the action type.
+Encoded as an array of shape ''(2,)'',
+in the form of [ 'coordinate_r', 'coordinate_c' ].
+"""
+
+@dataclass
+class OthelloState(BaseState):
+    """
+    ''OthelloState'' represents the game state.
+    """
+
+    board: NDArray[np.int_]
+    """
+    Array of shape ``(C, W, H)``,
+    where C is channel index
+    and W, H is board width, height.
+
+    Channels
+        - ''C = 0'': one-hot encoded position of agent 0. (black)
+        - ''C = 1'': one-hot encoded position of agent 1. (white)
+    """
+
+    done: bool = False
+    """
+    Boolean value indicating wheter the game is done.
+    """
